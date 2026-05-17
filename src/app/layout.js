e958db9687dded3a27a3c05d2fc3f7b30aa78bca
Saha-Ovi <ovisaha3210@gmail.com>
2026-05-18 02:18:50 +0600

    environment ready

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,20 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans, Playfair_Display, Roboto } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const roboto = Roboto({
+  variable:"--font-roboto",
+  subsets:["latin"],
+})
+const sansDM = DM_Sans({
+  variable:"--font-dm-sans",
+  subsets:["latin"],
+  
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const playFair = Playfair_Display({
+  variable:"--font-play-fair",
+  subsets:["latin"],
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -20,7 +25,8 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      data-theme="light"
+      className={`${sansDM.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
