c38e1c9fb79ff3380eca9771045a6ae99745a147
Saha-Ovi <ovisaha3210@gmail.com>
2026-05-18 23:07:49 +0600

    banner section added and responsive

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -1,10 +1,11 @@
+import Banner from '@/components/Banner.jsx/Banner';
 import Navbar from '@/components/Navbar/Navbar';
 import React from 'react';
 
-const MainLayout = () => {
+const MainLayout = ({children}) => {
     return (
         <div>
-            <Navbar/>
+            {children}
         </div>
     );
 };
--- src/components/Banner.jsx/Banner.jsx
+++ src/components/Banner.jsx/Banner.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import bannerImg from '../../../public/oofPicture.png'
+import Image from 'next/image';
+
 
 const Banner = () => {
     return (
-        <div>
-            
+        <div className='container mx-auto my-8 bg'>
+
+            <div className='bg-linear-to-r from-[#1A1410] to-[#1A1410] m-4 py-10 px-8 rounded-xl flex flex-col lg:flex-row items-center justify-between  gap-4 lg:gap-0'>
+                {/* left */}
+                <div className=''>
+                    <h2 className='text-sm md:text-md text-[#F5A623]'>☀️ Summer 2026 Collection</h2>
+                    <p className='text-white text-3xl md:text-5xl font-bold my-6'>Beat the Heat <br /> in <span className='text-[#F5A623]'>Style</span></p>
+                    <p className='text-sm text-[#FAF6F0]'>Sunglasses, Outfits, Skincare & Beach Accessories — all in one place.</p>
+                    <div className='mt-6 flex gap-6'>
+                        <button className='btn bg-[#F5A623] text-black border-none shadow-none rounded-full p-6 text-md'>Shop Now</button>
+                        <button className='btn bg-transparent text-white border-white shadow-none rounded-full p-6 text-md'>View Hot Deals 🔥</button>
+                    </div>
+                </div>
+                {/* right */}
+                <div className='w-full max-w-96 md:max-w-md lg:max-w-lg flex justify-center '>
+                    <Image src={bannerImg} alt='30% OFF' height={"500"} width={"500"} className='object-contain w-full h-auto rounded-xl '></Image>
+                </div>
+            </div>
+
         </div>
     );
 };
